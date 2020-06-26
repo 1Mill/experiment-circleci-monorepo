de54531eefa054d32793d094cb739e263cb910b2
1Mill <erik.ekberg@outlook.com>
2020-06-25 21:06:00 -0700

    Update build.bash

--- services/happy-day/build.bash
+++ services/happy-day/build.bash
@@ -1,6 +1,4 @@
 # ! bin/bash
-echo "Happy day! 1" &&
-echo "Happy day! 2" &&
+echo "Happy day! 1"
+echo "Happy day! 2"
 echo "Happy day! 3"
-
-echo "This should trigger a build of happy-days workflow"
