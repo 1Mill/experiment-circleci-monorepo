a6f2d5c3e73eea379b31bdf5bb010a23b10c4f14
1Mill <erik.ekberg@outlook.com>
2020-06-27 14:03:48 -0700

    Read in secrets

--- services/happy-day/index.js
+++ services/happy-day/index.js
@@ -1,1 +1,20 @@
-console.log('Hello world!');
+const fs = require('fs');
+
+let secure = {};
+try {
+	fs.readFile('./secrets.dev.sops.json', (err, data) => {
+		const secrets = JSON.parse(data.toString());
+
+		const {
+			example_key,
+			hello,
+		} = secrets;
+
+		console.log(hello);
+		console.log(example_key);
+	});
+	console.log('Success');
+} catch (err) {
+	console.error(err);
+	console.error('Unable to decode secrets');
+}
