ef019612c189979d56d9b67e58e77ef02980324f
1Mill <erik.ekberg@outlook.com>
2020-06-28 16:31:47 -0700

    Output testing value

--- organization/main.tf
+++ organization/main.tf
@@ -1,3 +1,11 @@
+data "sops_file" "secrets" {
+  	source_file = "secrets.sops.json"
+}
+
+output "testing" {
+	value = data.sops_file.secrets.data["testing"]
+}
+
 resource "aws_iam_group" "engineers" {
 	name = "engineers"
 }
