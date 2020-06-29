b7edc0ce04053c060bfbb1cdd9e6b0eea70faa11
1Mill <erik.ekberg@outlook.com>
2020-06-28 21:09:03 -0700

    Remove unnesseary vars

--- organization/main.tf
+++ organization/main.tf
@@ -1,9 +1,5 @@
 module "secrets" {
 	source = "./modules/secrets"
-
-	AWS_PROFILE = var.AWS_PROFILE
-	AWS_ACCESS_KEY_ID = var.AWS_ACCESS_KEY_ID
-	AWS_SECRET_ACCESS_KEY = var.AWS_SECRET_ACCESS_KEY
 }
 
 output "testing_value" {
--- organization/modules/secrets/main.tf
+++ organization/modules/secrets/main.tf
@@ -2,16 +2,6 @@ provider "sops" {
 	version = "~> 0.5.1"
 }
 
-variable "AWS_PROFILE" {
-	type = string
-}
-variable "AWS_ACCESS_KEY_ID" {
-	type = string
-}
-variable "AWS_SECRET_ACCESS_KEY" {
-	type = string
-}
-
 data "sops_file" "default" {
   	source_file = "${path.cwd}/secrets/secrets.sops.json"
 }
