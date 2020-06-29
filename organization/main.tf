8e1a7ceeae6572609fdb20c6ed53c50729fad6c6
1Mill <erik.ekberg@outlook.com>
2020-06-28 21:13:32 -0700

    Abstract (somewhat) how secrets are fetchehd

--- organization/main.tf
+++ organization/main.tf
@@ -1,9 +1,11 @@
 module "secrets" {
 	source = "./modules/secrets"
+
+	file_name = "secrets.sops.json"
 }
 
 output "testing_value" {
-	value = module.secrets.testing
+	value = module.secrets.data["testing"]
 }
 
 resource "aws_iam_group" "engineers" {
--- organization/modules/secrets/main.tf
+++ organization/modules/secrets/main.tf
@@ -1,11 +1,15 @@
+variable "file_name" {
+	type = string
+}
+
 provider "sops" {
 	version = "~> 0.5.1"
 }
 
 data "sops_file" "default" {
-  	source_file = "${path.cwd}/secrets/secrets.sops.json"
+  	source_file = "${path.cwd}/secrets/${var.file_name}"
 }
 
-output "testing" {
-	value = data.sops_file.default.data["testing"]
+output "data" {
+	value = data.sops_file.default.data
 }
