efaf78519059b66f67790236f5f98e4296405a1e
1Mill <erik.ekberg@outlook.com>
2020-07-01 20:44:26 -0700

    Move terraform-sops package to own repo

--- packages/docker/hub/1mill/terraform-sops/ci-deploy.bash
+++
@@ -1,5 +0,0 @@
-VERSION=`date +%Y-%m-%d_%H-%M-%S` && \
-docker build -t my-image . && \
-docker tag my-image 1mill/terraform-sops:${VERSION} && \
-docker push 1mill/terraform-sops:${VERSION} && \
-docker push 1mill/terraform-sops:latest
